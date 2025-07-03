6b1e18ec3c3894f43f54ac9dceec74c83f49cedf
RockyAtoyan <111198613+RockyAtoyan@users.noreply.github.com>
2025-07-03 18:39:27 +0300

    feat: create task form

---
+++ src/app/features/create-task-form/create-task-form.ts
@@ -0,0 +1,44 @@
+import { TaskApi } from './../../entities/task/api/api';
+import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { Input } from '../../shared/ui/input/input';
+import { Button } from '../../shared/ui/button/button';
+import { Input as InputDecorator } from '@angular/core';
+
+@Component({
+  selector: 'create-task-form',
+  imports: [ReactiveFormsModule, Input, Button],
+  templateUrl: './create-task-form.html',
+  styleUrl: './create-task-form.scss',
+})
+export class CreateTaskForm {
+  form = new FormGroup({
+    title: new FormControl('', Validators.required),
+    description: new FormControl(''),
+  });
+
+  @InputDecorator() onCreate?: () => void = () => {};
+
+  constructor(private taskApi: TaskApi) {}
+
+  onSubmit() {
+    console.log('Form submitted', this.form.value);
+    const data = this.form.value;
+    if (!data.title) {
+      this.form.setErrors({ title: 'Title is required' });
+      return;
+    }
+    this.taskApi.add({
+      title: data.title,
+      isCompleted: false,
+      description: data.description || '',
+    });
+    this.form.reset({ title: 'test', description: '' });
+    this.onCreate?.();
+  }
+}
