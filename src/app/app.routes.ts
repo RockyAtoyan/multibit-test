93b8a8d31eed632762b0871538ce6bf11ded97d3
RockyAtoyan <111198613+RockyAtoyan@users.noreply.github.com>
2025-07-03 18:41:52 +0300

    feat: app routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { MainLayout } from './layouts/main-layout/main-layout';
+import { Home } from './pages/home/home';
+import { TaskPage } from './pages/task-page/task-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'tasks',
+    pathMatch: 'full',
+  },
+  {
+    path: 'tasks',
+    component: MainLayout,
+    children: [
+      {
+        path: '',
+        component: Home,
+      },
+      {
+        path: ':id',
+        component: TaskPage,
+      },
+    ],
+  },
+];
