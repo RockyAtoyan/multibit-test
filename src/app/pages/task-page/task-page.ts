7620439b98a7090a015adf20fffc687951712bc4
RockyAtoyan <111198613+RockyAtoyan@users.noreply.github.com>
2025-07-03 18:40:57 +0300

    feat: task page

---
+++ src/app/pages/task-page/task-page.ts
@@ -0,0 +1,36 @@
+import { TaskApi } from './../../entities/task/api/api';
+import { Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+import { Checkbox } from '../../shared/ui/checkbox/checkbox';
+
+@Component({
+  selector: 'app-task-page',
+  imports: [Checkbox, RouterLink],
+  templateUrl: './task-page.html',
+  styleUrl: './task-page.scss',
+})
+export class TaskPage implements OnInit {
+  private activatedRoute = inject(ActivatedRoute);
+  private router = inject(Router);
+
+  taskId = this.activatedRoute.snapshot.params['id'];
+
+  taskApi = inject(TaskApi);
+
+  ngOnInit(): void {
+    const task = this.taskApi.getById(this.taskId);
+    if (!task) {
+      this.router.navigate(['/tasks']);
+      return;
+    }
+  }
+
+  toggleCompletion() {
+    const task = this.taskApi.getById(this.taskId);
+    console.log('task', task);
+    if (!task) return;
+    this.taskApi.update(task.id, {
+      isCompleted: !task.isCompleted,
+    });
+  }
+}
